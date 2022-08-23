33d8c8243b1568e9e97cb4b24368e49e1228880f
nicostray <nicolascorrea.alfaro@gmail.com>
2022-08-23 15:51:20 -0400

    cambios para deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    loader: "akamai",
+    path: ""
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  i18n: {
-    locales: ['es'],
-    defaultLocale: 'es',
   }
 }
 
